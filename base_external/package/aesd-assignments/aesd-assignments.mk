f57050d6f6532139f15f0f4435ae2f0ed79cc3cc
Ubuntu <ubuntu@ip-172-31-19-25.ec2.internal>
2023-06-10 08:26:20 +0000

    update for assignment 4-2

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 5d74a431a8aa8c5aeea3d53d16c7083a4057989f
+AESD_ASSIGNMENTS_VERSION = 6ac2893473c652ef60cf4f050dc2641cd43b8321
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
