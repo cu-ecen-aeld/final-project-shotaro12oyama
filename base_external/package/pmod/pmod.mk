70842396c415b8eb2439b10eab013b2637098cee
Ubuntu <ubuntu@ip-172-31-75-42.ec2.internal>
2023-10-07 01:18:11 +0000

    Sprint2: update and reorganize the driver file. moved to assignment3 repository

--- base_external/package/pmod/pmod.mk
+++ base_external/package/pmod/pmod.mk
@@ -1,15 +1,18 @@
-PMOD_SITE = $(TOPDIR)/../pmod
-PMOD_SITE_METHOD = local
+PMOD_VERSION = d182aae407c23cd6c26ffff131629d6d0d61ee6e
+PMOD_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-shotaro12oyama.git
+PMOD_SITE_METHOD = git
+PMOD_GIT_SUBMODULES = YES
+PMOD_MODULE_SUBDIRS = pmod
 
 $(eval $(kernel-module))
 
-define PMOD_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
-endef
 
 define PMOD_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/pmod $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/pmod_app-start-stop $(TARGET_DIR)/etc/init.d/S99pmod
+	mkdir -p $(BR2_ROOTFS_OVERLAY)/bin
+	$(INSTALL) -m 0755 $(PMOD_SRCDIR)/pmod/pmod_app.ko $(BR2_ROOTFS_OVERLAY)/bin/pmod_app.ko
+	$(INSTALL) -m 0755 $(PMOD_SRCDIR)/pmod/pmod_load $(BR2_ROOTFS_OVERLAY)/bin/pmod_load
+	$(INSTALL) -m 0755 $(PMOD_SRCDIR)/pmod/pmod_unload $(BR2_ROOTFS_OVERLAY)/bin/pmod_unload
+	sudo $(INSTALL) -m 0755 $(PMOD_SRCDIR)/pmod/pmod-start-stop $(BR@_ROOTFS_OVERLAY)/etc/init.d/S99pmod
 endef
 
 $(eval $(generic-package))
