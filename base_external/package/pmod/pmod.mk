b597992cffbbfd66a5920a7404c88ccc0d84443c
Ubuntu <ubuntu@ip-172-31-75-42.ec2.internal>
2023-10-15 22:54:33 +0000

    Sprint3: Update for wrap up

--- base_external/package/pmod/pmod.mk
+++ base_external/package/pmod/pmod.mk
@@ -1,4 +1,4 @@
-PMOD_VERSION = d182aae407c23cd6c26ffff131629d6d0d61ee6e
+PMOD_VERSION = af85e123d3e3514f9d91b056e3d8397f33a1989c
 PMOD_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-shotaro12oyama.git
 PMOD_SITE_METHOD = git
 PMOD_GIT_SUBMODULES = YES
@@ -6,13 +6,12 @@ PMOD_MODULE_SUBDIRS = pmod
 
 $(eval $(kernel-module))
 
-
 define PMOD_INSTALL_TARGET_CMDS
-	mkdir -p $(BR2_ROOTFS_OVERLAY)/bin
-	$(INSTALL) -m 0755 $(PMOD_SRCDIR)/pmod/pmod_app.ko $(BR2_ROOTFS_OVERLAY)/bin/pmod_app.ko
-	$(INSTALL) -m 0755 $(PMOD_SRCDIR)/pmod/pmod_load $(BR2_ROOTFS_OVERLAY)/bin/pmod_load
-	$(INSTALL) -m 0755 $(PMOD_SRCDIR)/pmod/pmod_unload $(BR2_ROOTFS_OVERLAY)/bin/pmod_unload
-	sudo $(INSTALL) -m 0755 $(PMOD_SRCDIR)/pmod/pmod-start-stop $(BR@_ROOTFS_OVERLAY)/etc/init.d/S99pmod
+	mkdir -p $(BR2_ROOTFS_OVERLAY)/usr/bin
+	$(INSTALL) -m 0755 $(PMOD_SRCDIR)/pmod/test.sh $(BR2_ROOTFS_OVERLAY)/usr/bin/test.sh
+	$(INSTALL) -m 0755 $(PMOD_SRCDIR)/pmod/pmod_app.ko $(BR2_ROOTFS_OVERLAY)/usr/bin/pmod_app.ko
+	$(INSTALL) -m 0755 $(PMOD_SRCDIR)/pmod/pmod_load $(BR2_ROOTFS_OVERLAY)/usr/bin/pmod_load
+	$(INSTALL) -m 0755 $(PMOD_SRCDIR)/pmod/pmod_unload $(BR2_ROOTFS_OVERLAY)/usr/bin/pmod_unload
 endef
 
 $(eval $(generic-package))
--- shared.sh
+++ shared.sh
@@ -2,6 +2,8 @@
 # Shared definitions for buildroot scripts
 
 BUILDROOT_EXTERNAL=../base_external
-PMOD_DEFCONFIG=${BUILDROOT_EXTERNAL}/configs/raspberrypi3_64_pmod_defconfig
+PMOD_DEFAULT_DEFCONFIG=/configs/raspberrypi3_defconfig
 PI_CONFIG_TXT=${BUILDROOT_EXTERNAL}/configs/config.txt
 PI_CMDLINE_TXT=${BUILDROOT_EXTERNAL}/configs/cmdline.txt
+PMOD_MODIFIED_DEFCONFIG=base_external/configs/pmod_defconfig
+PMOD_MODIFIED_DEFCONFIG_REL_BUILDROOT=../${PMOD_MODIFIED_DEFCONFIG}
